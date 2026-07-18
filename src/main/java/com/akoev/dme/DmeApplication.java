8396db0cb6724dda7cad49611311f585c37457e8
Angel Koev <106033028+angelkoev@users.noreply.github.com>
2026-07-18 10:11:03 +0300

    Initial Spring Boot skeleton from Spring Initializr
    Spring Boot 4.1.0, Java 25, with Data JPA / Security / WebMVC / Lombok starters.

---
+++ src/main/java/com/akoev/dme/DmeApplication.java
@@ -0,0 +1,13 @@
+package com.akoev.dme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akoev/dme/DmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akoev.dme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
